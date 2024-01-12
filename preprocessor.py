ec515bae0e0f95fcba4940ba89f071c2be91fadb
paras11917 <paras11917@gmail.com>
2024-01-12 14:51:16 +0530

    first commit

--- preprocessor.py
+++ preprocessor.py
@@ -2,14 +2,15 @@ import re
 import pandas as pd
 
 def preprocess(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    pattern = '\d{1,2}\/\d{2,4}\/\d{2,4},\s\d{1,2}:\d{1,2}\s\w{1,2}\s-\s'
 
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
 
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
     # convert message_date type
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%m/%d/%y, %H:%M %p - ')
+    # df['message_date'].strftime("%I:%M %p")
 
     df.rename(columns={'message_date': 'date'}, inplace=True)
 
